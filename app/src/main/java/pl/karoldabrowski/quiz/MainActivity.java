5adf1c51fe6e32efb9921d549984f59c5e6267b0
karol <kontakt@karoldabrowski.pl>
2018-03-23 18:59:37 +0100

    Initial commit

---
+++ app/src/main/java/pl/karoldabrowski/quiz/MainActivity.java
@@ -0,0 +1,72 @@
+package pl.karoldabrowski.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void quitGame(View view) {
+        finishAndRemoveTask();
+    }
+
+    public void starNewGame(View view) {
+        setContentView(R.layout.activity_main_game);
+    }
+
+    public void submitGame(View view) {
+        int points = 0;
+
+        EditText firstQuestionAnswerField = (EditText) findViewById(R.id.first_answer);
+        if (firstQuestionAnswerField.getText().toString().toLowerCase().equals("cat")) {
+            points++;
+        }
+
+        RadioGroup secondQuestionAnswerGroup = (RadioGroup) findViewById(R.id.question_2_group);
+        int selectedRadioButtonId = secondQuestionAnswerGroup.getCheckedRadioButtonId();
+        if (selectedRadioButtonId != -1) {
+            RadioButton selectedRadioButton = (RadioButton) findViewById(selectedRadioButtonId);
+            if (selectedRadioButton.getText().toString().equals("France")) {
+                points++;
+            }
+        }
+
+        CheckBox question3answer1 = (CheckBox) findViewById(R.id.question_3_checkbox_1);
+        CheckBox question3answer2 = (CheckBox) findViewById(R.id.question_3_checkbox_2);
+        CheckBox question3answer3 = (CheckBox) findViewById(R.id.question_3_checkbox_3);
+        CheckBox question3answer4 = (CheckBox) findViewById(R.id.question_3_checkbox_4);
+
+        if (question3answer1.isChecked() && !question3answer2.isChecked()
+                && !question3answer3.isChecked() && question3answer4.isChecked()) {
+            points++;
+        }
+
+        RadioGroup fourthQuestionAnswerGroup = (RadioGroup) findViewById(R.id.question_4_group);
+        selectedRadioButtonId = fourthQuestionAnswerGroup.getCheckedRadioButtonId();
+        if (selectedRadioButtonId != -1) {
+            RadioButton selectedRadioButton = (RadioButton) findViewById(selectedRadioButtonId);
+            if (selectedRadioButton.getText().toString().equals("Apple")) {
+                points++;
+            }
+        }
+
+        setContentView(R.layout.activity_main_result);
+        TextView result = (TextView) findViewById(R.id.score_text_view);
+        result.setText(points + "/4");
+    }
+
+    public void backToMenu(View view) {
+        setContentView(R.layout.activity_main);
+    }
+}
